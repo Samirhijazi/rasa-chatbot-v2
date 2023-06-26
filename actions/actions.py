5bda461eebe9414a89d58ec78ae403ad181796bc
samir hijazi <73133915+Samirhijazi@users.noreply.github.com>
2023-06-26 15:08:33 +0300

    Update actions.py

--- actions/actions.py
+++ actions/actions.py
@@ -4,6 +4,18 @@ from datetime import datetime, timedelta
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.types import DomainDict
+from rasa_sdk.forms import FormValidationAction
+from rasa.core.agent import Agent
+import dateutil.parser
+import spacy
+import re
+
+nlp = spacy.load("en_core_web_sm")
+
+DEGREE_KEYWORDS = ["degrees"]
+TIME_KEYWORDS = ["seconds", "second", "minutes", "minute", "hours", "hour"]
 
 
 def text_date_to_int(text_date):
@@ -13,7 +25,6 @@ def text_date_to_int(text_date):
         return 1
     if text_date == "yesterday":
         return -1
-
     # in other case
     return None
 
@@ -96,48 +107,107 @@ class ActionQueryWeekday(Action):
 
 
 
+# class ActionQueryBot(Action):
+#     def name(self) -> Text:
+#         return "action_query_bot"
+    
+#     def run(
+#             self,
+#             dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any],
+#     ) -> List[Dict[Text, Any]]:
+        # intent = tracker.latest_message.get("intent", {}).get("name")
+        # intent = tracker.latest_message.get("text", {}).get("name")
+        # entities = tracker.latest_message.get("entities", [])
+        # entity_info = [f"Entity: {entity['entity']} [{entity['value']}]" for entity in entities]
+        # degree_entity = next(
+        #     (entity for entity in entities if entity["entity"] == "degree"), None
+        # )
+        # time_entity = next(
+        #     (entity for entity in entities if entity["entity"] == "time"), None
+        # )
+
+        # print("Degree Entity:", degree_entity)
+        # print("Time Entity:", time_entity)
+
+        # dispatcher.utter_message(
+        #         text=f"The response."
+        #     )
+        # return []
+
+        # if not degree_entity:
+        #     missing_entity = "degree"
+        #     dispatcher.utter_message(
+        #         text=f"The entity '{missing_entity}' is missing. Please provide the value for '{missing_entity}'."
+        #     )
+        #     return [SlotSet("requested_slot", "degree")]
+
+        # if not time_entity:
+        #     missing_entity = "time"
+        #     dispatcher.utter_message(
+        #         text=f"The entity '{missing_entity}' is missing. Please provide the value for '{missing_entity}'."
+        #     )
+        #     return [SlotSet("requested_slot", "time")]
+
+        # degree_value = degree_entity["value"]
+        # time_value = time_entity["value"]
+
+        # # Handle degree entity
+        # degree_info = process_degree_value(degree_value)
+        # dispatcher.utter_message(text=f"Degree Entity: {degree_entity['entity']} - {degree_info}")
+
+        # # Handle time entity
+        # time_info = resolve_time(time_value)
+        # dispatcher.utter_message(text=f"Time Entity: {time_entity['entity']} - {time_info}")
+
+        # return [
+        #     SlotSet("degree_value", degree_info),
+        #     SlotSet("time_value", time_info),
+        # ]
+
+import requests
 class ActionQueryBot(Action):
     def name(self) -> Text:
-        return "action_query_bot"
+        return "action_bot_connect"
     
     def run(
             self,
             dispatcher: CollectingDispatcher,
             tracker: Tracker,
-            domain: Dict[Text, Any],
+            domain: Dict[Text, Any]
     ) -> List[Dict[Text, Any]]:
-        intent = tracker.latest_message.get("intent", {}).get("name")
-        entities = tracker.latest_message.get("entities", [])
-        entity_info = [f"Entity: {entity['entity']} [{entity['value']}]" for entity in entities]
-        degree_entity = next(
-            (entity for entity in entities if entity["entity"] == "degree{45}"), None
-        )
-        time_entity = next(
-            (entity for entity in entities if entity["entity"] == "time{5}"), None
-        )
+        user_message = tracker.latest_message.get("text")
+        dispatcher.utter_message(text=f"message: {user_message} llll")
 
-        if degree_entity and degree_entity['value']:
-            degree_value = degree_entity['value']
-            dispatcher.utter_message(text=f"{degree_entity['entity']} : {degree_value}")
-            if time_entity and time_entity['value']:
-                time_value = time_entity['value']
-                dispatcher.utter_message(text=f"{time_entity['entity']} : {time_value}")
-                return [
-                    SlotSet("degree_value", degree_value),
-                    SlotSet("time_value", time_value),
-                ]
-            else:
-                missing_entity = "time"
-                dispatcher.utter_message(
-                    text=f"The entity '{missing_entity}' is missing. Please provide the value for '{missing_entity}'."
-                )
-                return [SlotSet("requested_slot", missing_entity)]
-        else:
-            missing_entity = "degree"
-            dispatcher.utter_message(
-                text=f"The entity '{missing_entity}' is missing. Please provide the value for '{missing_entity}'."
-            )
-            return [SlotSet("requested_slot", missing_entity)]
+        # Print the bot's response
+        # for message in bot_response:
+        #     print(message["text"])
+        #     dispatcher.utter_message(
+        #         text=f"The text is '{message['text']}'."
+        #     )
+
+def process_degree_value(degree_value: str) -> str:
+    # Remove the 'degrees' keyword from the value
+    degree_value = degree_value.replace("degrees", "").strip()
+    return degree_value
+
+def resolve_time(time_value: str) -> str:
+    # Check if the value contains the word 'second' or 'seconds'
+    if "second" in time_value:
+        return "00:00:" + re.findall(r"\d+", time_value)[0].zfill(2)
+
+    # Check if the value contains the word 'minute' or 'minutes'
+    if "minute" in time_value:
+        minutes = re.findall(r"\d+", time_value)[0]
+        return "00:" + minutes.zfill(2) + ":00"
+
+    # Check if the value contains the word 'hour' or 'hours'
+    if "hour" in time_value:
+        hours = re.findall(r"\d+", time_value)[0]
+        return hours.zfill(2) + ":00:00"
+
+    return "Invalid time format"
 
         # if degree_entity is not None and degree_entity['value']:
         #     dispatcher.utter_message(text=f"intent: {intent} , [ {degree_entity['entity']} : {degree_entity['value']} ]")
